938cd6452f1cfb221eb21d828c59199c220c9ee8
GD <nannaya2121@hanmail.net>
2022-05-01 09:18:10 +0900

    pro_Traffic config First commit

---
+++ src/main/java/org/go/traffic/ProTrafficApplication.java
@@ -0,0 +1,13 @@
+package org.go.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTrafficApplication.class, args);
+	}
+
+}
