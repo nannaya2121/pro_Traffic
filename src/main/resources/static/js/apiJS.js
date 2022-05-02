bc8bd89c9f89ea8d26f82dbb0227dab7f9c6551d
GD <nannaya2121@hanmail.net>
2022-05-02 22:32:59 +0900

    연도, 지역, 상세지역 검색 function 완료

--- src/main/resources/static/js/apiJS.js
+++ src/main/resources/static/js/apiJS.js
@@ -2,61 +2,97 @@
 $(function() {
 
     var testBox = $('#testBox');
-    var resultBox = $('#resultBox');
     $('#searchBtn').click(function(){
 		var searchYear = $('#searchYear').val();
 		var city_value = $('#city').val();
-		
+		var gugun_value = $('#gugun').val(); 
 		
 		console.log('searchYear 값 : ' + searchYear
-					+ '\n city_value 값 : ' + city_value);
+					+ '\n city_value 값 : ' + city_value
+					+ '\n gugun_value 값 : ' + gugun_value);
 	
-	
-       	/*testBox.html('');
-        var searchYear = $('#searchYear').val();
+		if(searchYear == '-1'){
+			alert('조회할 연도를 선택해주세요');
+			return;
+		}
+		
+		if(city_value == '-1'){
+			alert('조회지역을 선택해주세요');
+			return;
+		}
+		
+		if(gugun_value == '-1'){
+			alert('상세지역을 선택해주세요');
+			return;
+		}
+		
+		
+       	testBox.html('');
 
         var url = 'http://apis.data.go.kr/B552061/AccidentDeath/getRestTrafficAccidentDeath';
         var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'cfWiR1HunYkkDf7oGcdyeFa0nJA7C4sFUKo9AEZIEaFLXyZirmI%2BeKZf2s7BDV8VaLwGZB%2Bstjl%2B7hx%2BJJ%2Fb0Q%3D%3D';
         queryParams += '&' + encodeURIComponent('searchYear') + '=' + encodeURIComponent(searchYear);
         queryParams += '&' + encodeURIComponent('siDo') + '=' + encodeURIComponent(city_value);
-        queryParams += '&' + encodeURIComponent('guGun') + '=' + encodeURIComponent('1117');
+        queryParams += '&' + encodeURIComponent('guGun') + '=' + encodeURIComponent(gugun_value);
         queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json');
-        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('20');
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('50');
         queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
     
         var totalURL = url + queryParams;
-       
+       console.log('totalURL : ' + totalURL);
         $.ajax({
             url: totalURL,
             type: "GET", 
             success: function(data) {
-                alert(Object.keys(data.items.item).length);
+				/*총 결과 개수
+                alert(Object.keys(data.items.item).length);*/
                 var testObj = '<div>'
+				testObj += '총 ' + Object.keys(data.items.item).length + '건 <br>';
                 $(data.items.item).each(function(){
-                    
+					testObj += dateFormatter(this.occrrnc_dt) +'&emsp; <a href="#">상세보기</a>';
                    testObj += '위도 좌표 : '+this.la_crd + ' ||| 경도 좌표 : '+this.lo_crd + '<br></div>';
                 });
                 testBox.append(testObj);
             }
-        });*/
+        });
     });
 });
 
-function changeCity(){
+function changeCity(city_value){
+	var gugun = $('#gugun');
+	var optionTag = '<option value="-1">상세지역 선택</option>';
 	
-	var cityListValue = $('#cityListValue').val();
-	alert(cityListValue);
-	
-	console.log('e 값 확인 : ' + cityListValue + '\n');
+	console.log('cityListValue 값 확인 : ' + city_value + '\n');
 		$.ajax({
 		url: '/traffic/gugunSearchList',
 		type: "POST", 
-		data: {city_value:cityListValue},
+		data: {city_value:city_value},
 		success: function(data) {
+			gugun.empty();
 			$(data).each(function(){
-				console.log(this.gugun_name + '\n');
+				optionTag += '<option value="'+this.gugun_value+'">'
+				+this.gugun_name+'</option>';
 			});
+			gugun.append(optionTag);
         }
     });
+}
+
+function dateFormatter(num){
+
+     if(!num) return "";
+     var formatNum = '';
+
+     // 공백제거
+     num=num.replace(/\s/gi, "");
+     try{
+          if(num.length == 10) {
+               formatNum = num.replace(/(\d{4})(\d{2})(\d{2})(\d{2})/, '$1년 $2월 $3일 $4시');
+          }
+     } catch(e) {
+          formatNum = num;
+          console.log(e);
+     }
+     return formatNum;
 }
 
