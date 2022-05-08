eaa6c3498c65b0e085699c3c90bfed3eb067a5fa
GD <nannaya2121@hanmail.net>
2022-05-09 08:31:29 +0900

    conflict

--- src/main/java/org/go/traffic/controller/AdminController.java
+++ src/main/java/org/go/traffic/controller/AdminController.java
@@ -1,18 +1,7 @@
 package org.go.traffic.controller;
 
-import java.util.List;
-
-import org.go.traffic.model.CityDTO;
-import org.go.traffic.model.GugunDTO;
-import org.go.traffic.service.CityService;
-import org.go.traffic.service.GugunService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
