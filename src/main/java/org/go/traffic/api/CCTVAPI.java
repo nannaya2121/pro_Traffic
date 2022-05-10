73bc2d5ecd43b120148ac159c002c8f04479dc2f
GD <nannaya2121@hanmail.net>
2022-05-10 17:50:47 +0900

    CCTV API 불러오기

---
+++ src/main/java/org/go/traffic/api/CCTVAPI.java
@@ -0,0 +1,59 @@
+package org.go.traffic.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class CCTVAPI {
+	
+	public static JSONObject cctvCall() throws IOException{
+		  StringBuilder urlBuilder = new StringBuilder("https://openapi.its.go.kr:9443/cctvInfo"); /*URL*/
+		  urlBuilder.append("?" + URLEncoder.encode("apiKey", "UTF-8") + "=" + URLEncoder.encode("44f4eef6797d479f9a50e151ff333872", "UTF-8")); /*공개키*/
+		  urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("ex", "UTF-8")); /*도로유형*/
+		  urlBuilder.append("&" + URLEncoder.encode("cctvType","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*CCTV유형*/
+		  urlBuilder.append("&" + URLEncoder.encode("minX","UTF-8") + "=" + URLEncoder.encode("126.800000", "UTF-8")); /*최소경도영역*/
+		  urlBuilder.append("&" + URLEncoder.encode("maxX","UTF-8") + "=" + URLEncoder.encode("127.890000", "UTF-8")); /*최대경도영역*/
+		  urlBuilder.append("&" + URLEncoder.encode("minY","UTF-8") + "=" + URLEncoder.encode("34.900000", "UTF-8")); /*최소위도영역*/
+		  urlBuilder.append("&" + URLEncoder.encode("maxY","UTF-8") + "=" + URLEncoder.encode("35.100000", "UTF-8")); /*최대위도영역*/
+		  urlBuilder.append("&" + URLEncoder.encode("getType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*출력타입*/
+		  URL url = new URL(urlBuilder.toString());
+		  HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		  conn.setRequestMethod("GET");
+		  conn.setRequestProperty("Content-type", "text/xml;charset=UTF-8");
+		  System.out.println("Response code: " + conn.getResponseCode());
+		  BufferedReader rd;
+		  if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+		   rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		  } else {
+		   rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		  }
+		  StringBuilder sb = new StringBuilder();
+		  String line;
+		  while ((line = rd.readLine()) != null) {
+		   sb.append(line);
+		  }
+		  rd.close();
+		  conn.disconnect();
+		  System.out.println(sb.toString());
+		  
+		  String jsonStr=sb.toString();//sb는 StringBuilder 객체. API의response를 통해 값을 받은 상태. 
+			JSONParser parser = new JSONParser();
+			Object obj;
+			try {
+				obj = parser.parse(jsonStr);
+				JSONObject jsonObj = (JSONObject) obj;
+				return jsonObj;
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			return null;
+	}
+
+}
--- src/main/java/org/go/traffic/controller/MainController.java
+++ src/main/java/org/go/traffic/controller/MainController.java
@@ -1,15 +1,33 @@
 package org.go.traffic.controller;
 
+import java.io.IOException;
+
+import org.go.traffic.api.CCTVAPI;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         System.out.println("index Access Success");
         
+        try {
+			JSONObject result = CCTVAPI.cctvCall();
+			System.out.println("CCTV Result : " + result); 
+			
+			System.out.println("ttttttt >> : " + result.get("response.coordtype"));
+			
+			
+			//model.addAttribute("apiResult",result);
+			
+			return "index";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         return "index";
     }
 }
