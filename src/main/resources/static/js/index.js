fe74b666b5a90e06bb8bc573fbfacf2389c62385
GD <nannaya2121@hanmail.net>
2022-05-13 17:48:56 +0900

    CCTV ing

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,8 @@
+$(function(){
+		 
+	$(window).resize(function() {        
+	    $('body').css('width', $(window).width() );        
+	    $('body').css('height', $(window).height() );   
+	});
 
+});
